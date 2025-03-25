e1b802ed3d8fe920014d41985447cf7f66fbb10d
Veronica Lyu <24516605@student.uwa.edu.au>
2025-03-25 21:55:52 +0800

    use java.math.BigInteger class deal with 30!

--- lab/3/Exercise_2/Factorials.java
+++ lab/3/Exercise_2/Factorials.java
@@ -1,16 +1,26 @@
 //package lab.3.Exercise 2;
 import java.util.Scanner;
+import java.math.BigInteger;
+
 
 public class Factorials {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);   
     System.out.println("Input a number which you want to know its factorials:");
     int num = sc.nextInt();
-    int fac=1;
-    for (int i=1; i<=num; i++){
-        fac = fac * i;
+    double fac=1;
+    if (num<18){
+      for (int i=1; i<=num; i++){
+          fac = fac * i;
+          //System.out.println(fac);
+      }
+      System.out.println("The number "+ num+" is "+fac);
+    } else {
+      BigInteger bi = BigInteger.ONE;
+      for (int i = 1; i <= num; i++) {
+        bi = bi.multiply(BigInteger.valueOf(i));
+      }
+    System.out.println("The number "+ num+" is "+bi);
     }
-    System.out.println("The number "+ num+" is "+fac);
-
   }
 }
